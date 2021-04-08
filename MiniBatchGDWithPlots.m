8ba8eda4fed98b6d5e5955da60154c24974c2ec3
GillesVanDeVyver <gillesvandevyver@hotmail.be>
2021-04-08 23:03:43 +0200

    Corrections on cyclic learning rate

--- MiniBatchGDWithPlots.m
+++ MiniBatchGDWithPlots.m
@@ -4,43 +4,53 @@ function [Wstar, bstar] = MiniBatchGDWithPlots(X, Y, XValid, YValid, GDparams, W
     Wstar=W;
     bstar=b;
     n = size(X,2);
-    total_nb_steps = GDparams.nb_cycles*n_s*2;
-    costTrain = zeros(total_nb_steps+1,1);
-    costValid = zeros(total_nb_steps+1,1);
+    eval_per_cycle = 10;
+    nb_eval_points = GDparams.nb_cycles*eval_per_cycle;
+    eval_interval = floor((2*n_s)/eval_per_cycle);
+    costTrain = zeros(nb_eval_points+1,1);
+    costValid = zeros(nb_eval_points+1,1);
     costTrain(1) = ComputeCost(X, Y, Wstar, bstar,GDparams.lambda);
     costValid(1) = ComputeCost(XValid, YValid, Wstar, bstar,GDparams.lambda);
     nb_layers = size(W,2);
     t=0;
+    eval_step = 2;
+    batch_offset = 1;
     for l=0:GDparams.nb_cycles-1
-        t
         while t<(2*l+1)*n_s
             eta = GDparams.eta_min+(t-2*l*n_s)/n_s*(GDparams.eta_max-GDparams.eta_min);
-            [Wstar,bstar] = update(eta,n,X,Y,Wstar,bstar,GDparams,nb_layers);
-            costTrain(t+1) = ComputeCost(X, Y, Wstar, bstar,GDparams.lambda);
-            costValid(t+1) = ComputeCost(XValid, YValid, Wstar, bstar,GDparams.lambda);
+            [Wstar,bstar,batch_offset] = update(eta,n,X,Y,Wstar,bstar,GDparams,nb_layers,batch_offset);
             t=t+1;
+            if mod(t,eval_interval) == 0
+                costTrain(eval_step) = ComputeCost(X, Y, Wstar, bstar,GDparams.lambda);
+                costValid(eval_step) = ComputeCost(XValid, YValid, Wstar, bstar,GDparams.lambda);
+                eval_step = eval_step+1;
+                t
+            end
+            
         end
-        t
         while t<2*(l+1)*n_s
             eta= GDparams.eta_max-(t-(2*l+1)*n_s)/n_s*(GDparams.eta_max-GDparams.eta_min);
-            [Wstar,bstar] = update(eta,n,X,Y,Wstar,bstar,GDparams,nb_layers);
-            costTrain(t+1) = ComputeCost(X, Y, Wstar, bstar,GDparams.lambda);
-            costValid(t+1) = ComputeCost(XValid, YValid, Wstar, bstar,GDparams.lambda);
+            [Wstar,bstar,batch_offset] = update(eta,n,X,Y,Wstar,bstar,GDparams,nb_layers,batch_offset);
             t=t+1;
+            if mod(t,eval_interval) == 0
+                costTrain(eval_step) = ComputeCost(X, Y, Wstar, bstar,GDparams.lambda);
+                costValid(eval_step) = ComputeCost(XValid, YValid, Wstar, bstar,GDparams.lambda);
+                eval_step = eval_step+1;
+                t
+            end
+            
         end
     end
-    x_axis = 0:total_nb_steps;
+    x_axis = 0:nb_eval_points;
+    x_axis = x_axis*eval_interval;
     figure
-    whos x_axis
-    whos costTrain
-    whos costValid
     plot(x_axis,costTrain,x_axis,costValid)
+    ylim([0 inf])
     xlabel('update step') 
     ylabel('loss')
     legend({'training loss','validation loss'},'Location','northeast')
     title(plotTitle)
-    axis tight
-    print -depsc cost_exercise_4
+    print -depsc cost_exercise_3
 end
 
 
