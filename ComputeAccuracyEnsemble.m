60c79d8e95298b5385342e8aba87f9bc64a5f6fb
GillesVanDeVyver <gillesvandevyver@hotmail.be>
2021-04-12 22:12:22 +0200

    Final assignment 2 bonus correction

--- ComputeAccuracyEnsemble.m
+++
@@ -1,5 +0,0 @@
-function acc = ComputeAccuracyEnsemble(X, y, W_enseble, b_ensemble)
-    P = EvaluateClassifierEnsemble(X, W_enseble, b_ensemble);
-    [~,I] = max(P);
-    acc = sum(permute(I,[2,1])==y)/size(X,2);
-end
